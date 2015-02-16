6c04d2677076122943cf01d4a5c86436692259d8
Asymons <alessiosymons@gmail.com>
2015-02-16 14:40:34 -0500

    updating to beta of robot

---
+++ src/org/usfirst/frc/team4716/robot/commands/MoveMotorBack.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team4716.robot.commands;
+
+import org.usfirst.frc.team4716.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveMotorBack extends Command {
+
+    public MoveMotorBack() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.elevator);
+    	setTimeout(2.5);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.elevator.moveElevCIM(-0.6);
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.elevator.moveElevCIM(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.elevator.moveElevCIM(0);
+    }
+}
